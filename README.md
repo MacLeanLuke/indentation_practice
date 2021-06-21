e48034d6e700900c03ab4f4e66af7e60fa985924
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-06-21 14:41:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+This is the read me for my first Coding Dojo project.
---
+++ script.js
@@ -0,0 +1,8 @@
+x=10;
+console.log(5*x);
+
+document.write("hello galatic empire.")
+
+for(var i=0; i<70; i++){
+    console.log('i',i);
+}
